2ce84ec408bd6f1a010fc8cfd7687ab7482b7852
sohan004 <aryansohan02@gmail.com>
2023-05-28 13:19:29 +0600

    key fetures section and teammembers section added

--- src/components/TeamMember/TeamMember.jsx
+++ src/components/TeamMember/TeamMember.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+
+import image from '../../assets/architect.png'
+import MemberCard from './MemberCard';
+import './TeamCard.css'
 
 const TeamMember = () => {
+  const members = [
+    {
+      id: 1,
+      name: "name",
+      img: image,
+      position: "team position"
+    },
+    {
+      id: 2,
+      name: "name",
+      img: image,
+      position: "team position"
+    },
+    {
+      id: 3,
+      name: "name",
+      img: image,
+      position: "team position"
+    },
+    {
+      id: 4,
+      name: "name",
+      img: image,
+      position: "team position"
+    }
+  ];
+
   return (
-    <div>TeamMember</div>
+    <div>
+      <h3 className="text-center mt-14 text-3xl font-semibold text-cyan-600">Our Strong Team</h3>
+      <p className="text-center mt-1 mb-3 text-lg ">It is a strong pillar</p>
+      <div className='max-w-screen-xl p-4 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7'>
+        <div className='md:col-span-2 flex items-center'>
+          <div className=''>
+            <p className='mb-3  inline  bg-clip-text text-transparent bg-gradient-to-r from-indigo-700 via-purple-500 to-pink-500'>Team Member</p>
+            <h3 className='text-2xl md:text-4xl'>Our proffessional </h3>
+            <h3 className='text-2xl md:text-4xl'>expert team members </h3>
+          </div>
+        </div>
+        <div  className="relative">
+            <img src={image} className="w-full rounded-lg" alt="" />
+            <div style={{ borderRadius: '0 140px 0px 0' }} className={` left-0 bg-opacity-90 bottom-0 absolute bg-slate-800 text-white p-12`}>
+                <h2 className="text-xl font-semibold mb-2">sohan</h2>
+                <p>team position</p>
+            </div>
+            <div style={{ borderRadius: '0 140px 0px 0', padding: '65px 102px' }} className={` left-0  border-r-4 bg-opacity-90 bottom-0 absolute  text-white `}>
+
+            </div>
+        </div>
+        {members.map(m => <MemberCard key={m.id} m={m}></MemberCard>)}
+      </div>
+
+    </div>
   )
 }
 
